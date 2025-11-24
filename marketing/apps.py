ed6c5f6650608f159f510a062cb18f48c71ad0df
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-11-23 19:35:40 -0500

    email

--- marketing/apps.py
+++ marketing/apps.py
@@ -1,6 +1,34 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class MarketingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketing'
+
+    def ready(self):
+        from django.db import OperationalError, ProgrammingError
+        from django.db.models.signals import post_save
+
+        from .models import MarketingSettings
+
+        def _apply(instance: MarketingSettings):
+            if not instance or not instance.has_smtp_credentials():
+                return
+            config = instance.smtp_config()
+            for key, value in config.items():
+                setattr(settings, key, value)
+
+        def _initialize():
+            try:
+                instance = MarketingSettings.objects.order_by("pk").first()
+            except (OperationalError, ProgrammingError):
+                return
+            if instance:
+                _apply(instance)
+
+        def _handle_save(sender, instance, **kwargs):
+            _apply(instance)
+
+        _initialize()
+        post_save.connect(_handle_save, sender=MarketingSettings, dispatch_uid="marketing_apply_email_settings")
